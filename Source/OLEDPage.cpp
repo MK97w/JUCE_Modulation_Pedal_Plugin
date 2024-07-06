fc40af70bd4eeca2a4a66b8a90f3e377ecacdc95
MK97w <kabukcuoglumert@gmail.com>
2024-07-06 17:55:43 +0300

    thats all for today folks

--- Source/OLEDPage.cpp
+++ Source/OLEDPage.cpp
@@ -8,4 +8,39 @@
   ==============================================================================
 */
 
+
 #include "OLEDPage.h"
+
+namespace juce
+{
+    OLEDPage::OLEDPage() {}
+
+    void OLEDPage::setFont(const Font& font)
+    {
+        for (auto* label : labels)
+        {
+            label->setFont(font);
+        }
+    }
+
+    void OLEDPage::addLabel(const String& text, const Rectangle<int>& bounds)
+    {
+        auto* label = new Label();
+        label->setText(text,dontSendNotification);
+        label->setBounds(bounds);
+        labels.add(label);
+    }
+
+    void OLEDPage::clearLabels()
+    {
+        labels.clear();
+    }
+
+    void OLEDPage::addToComponent(Component& parent)
+    {
+        for (auto* label : labels)
+        {
+            parent.addAndMakeVisible(label);
+        }
+    }
+}
--- Source/OLEDPage.h
+++ Source/OLEDPage.h
@@ -7,5 +7,39 @@
 
   ==============================================================================
 */
-
 #pragma once
+
+#include <JuceHeader.h>
+
+namespace juce
+{
+    class OLEDPage
+    {
+    public:
+        OLEDPage();
+        void setFont(const Font& font);
+        void addLabel(const String& text, const Rectangle<int>& bounds);
+        void clearLabels();
+        void addToComponent(Component& parent);
+
+    private:
+        juce::OwnedArray<Label> labels;
+
+
+    public:
+         class CustomFontLookAndFeel : public juce::LookAndFeel_V4
+         {
+         public:
+            CustomFontLookAndFeel()
+            {
+                LookAndFeel::setDefaultLookAndFeel(this);
+            }
+            static const juce::Font getCustomFont()
+            {
+                static auto typeface = juce::Typeface::createSystemTypefaceFor(BinaryData::munro_ttf, BinaryData::munro_ttfSize);
+                return juce::Font(typeface);
+            }
+          private:
+          }customFontLookAndFeel;
+    };
+}
