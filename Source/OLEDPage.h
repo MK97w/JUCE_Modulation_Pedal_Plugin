ec29b45ad56b96b56e677f4b36917ebd69e1a528
MK97w <kabukcuoglumert@gmail.com>
2024-11-12 16:46:56 +0300

    something is happening with oledpage

--- Source/OLEDPage.h
+++ Source/OLEDPage.h
@@ -11,40 +11,58 @@
 #pragma once
 
 #include <JuceHeader.h>
+/***
+*
+* Notes on design:
+*   - I think pages shouldnt be the listeners themselves -> they just display
+*   - Base class must contain a "setPageLayout" type of function. -> Draw lines / set Title hence a variable juce::String title but this should be called only when page layout is changed.
+*   - ýnstead of taking the whole apvts, create a hashamp that contains the sub-effects of apvts and pass it to display
+* 
+*/
 
-class OLEDPage : public juce::Component , 
-                 public juce::Button::Listener ,
-                 public juce::Slider::Listener
+using apvtsInfo = std::unordered_map<std::string, std::vector<juce::RangedAudioParameter*>>;
+
+
+class OLEDPage : public juce::Component 
 {
 
 protected:
     juce::Font customFont;
     juce::Rectangle<int> pageBounds;
-    enum class EffectType 
+    juce::String pageTitle;
+	apvtsInfo apvts;
+
+    static const juce::Font getCustomFont()
     {
-        VIBRATO,
-        PHASOR,
-        AUTO_WAH
-		//will increase in future
-    };
+        static auto typeface = juce::Typeface::createSystemTypefaceFor(BinaryData::munro_ttf, BinaryData::munro_ttfSize);
+        return juce::Font(typeface);
+    }
 
 public:
-    OLEDPage(juce::AudioProcessorValueTreeState& apvts, juce::Graphics& g);
-    virtual ~OLEDPage();
-    virtual void createPage(EffectType selectedEffect) = 0;
-    virtual juce::String get_APVTS(EffectType) = 0;
-    void paint(juce::Graphics& g) override;
+    OLEDPage(apvtsInfo& info) : apvts(info), customFont(getCustomFont()) {};
+    virtual ~OLEDPage(); 
 
+    void setFontSize(float newSize) { customFont.setHeight(newSize); }
+    void setFontBold(bool shouldBeBold) { customFont.setBold(shouldBeBold); }
+    void setFontItalic(bool shouldBeItalic) { customFont.setItalic(shouldBeItalic); }
+    void setFontTypeface(const juce::String& typefaceName) { customFont.setTypefaceName(typefaceName); }
+
+    void paint(juce::Graphics& g) override;
 };
 
 
 class EditPage : public OLEDPage
 {
-
+public:
+    EditPage(apvtsInfo&);
+    void paint(juce::Graphics& g) override;
 };
 
 class BasicEditPage : public OLEDPage
 {
+public:
+    BasicEditPage(apvtsInfo&);
+    void paint(juce::Graphics& g) override;
 };
 
 class PresetPage : public OLEDPage
