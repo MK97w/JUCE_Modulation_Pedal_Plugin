58045e3e1c5c784c16a537cc8e8f774d781bbe6b
MK97w <kabukcuoglumert@gmail.com>
2024-12-11 20:52:24 +0300

    let him cook

--- Source/vibrato.h
+++ Source/vibrato.h
@@ -19,4 +19,14 @@ public:
     std::unique_ptr<juce::AudioProcessorParameterGroup> createVibratoParameterGroup();
     void prepareToPlay(double sampleRate, int samplesPerBlock);
 	void processBlock(juce::AudioSampleBuffer& buffer, juce::MidiBuffer& midiMessages);
+
+private:
+	juce::AudioBuffer<float> delayBuffer;
+    struct lfo
+    {
+		float operator()(float phase, int waveform)
+		{
+
+		}
+    };
 };
