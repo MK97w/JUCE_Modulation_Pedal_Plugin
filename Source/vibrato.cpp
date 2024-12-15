362e25fa5ef0ae4c8166abe584ea0a32d28a40d2
MERT <kabukcuoglumert@gmail.com>
2024-12-15 21:54:50 +0300

    unfinished business

--- Source/vibrato.cpp
+++ Source/vibrato.cpp
@@ -16,10 +16,35 @@ std::unique_ptr<juce::AudioProcessorParameterGroup> Vibrato::createVibratoParame
     vibratoGroup->addChild(std::make_unique<juce::AudioParameterFloat>("_Vibrato_Intensity1", "INTENSITY", 0.1f, 10.0f, 5.0f));
     vibratoGroup->addChild(std::make_unique<juce::AudioParameterFloat>("_Vibrato_Elevel2", "EFFECT LEVEL", 0.0f, 1.0f, 0.5f));
     vibratoGroup->addChild(std::make_unique<juce::AudioParameterFloat>("_Vibrato_Dlevel3", "DIRECT LEVEL", 0.0f, 1.0f, 0.5f));
-    vibratoGroup->addChild(std::make_unique<juce::AudioParameterInt>("_Vibrato_WF4", "WAVEFORM",  1, 4, 1));
-    vibratoGroup->addChild(std::make_unique<juce::AudioParameterFloat>("Vibrato_Rate5", "RATE", 0.1f, 10.0f, 5.0f));
-    vibratoGroup->addChild(std::make_unique<juce::AudioParameterInt>("Vibrato_Complexity6", "COMPLEXITY", 1, 4, 1));
-    vibratoGroup->addChild(std::make_unique<juce::AudioParameterFloat>("Vibrato_G", "Vibrato G", 0.0f, 1.0f, 0.5f));
-
+    vibratoGroup->addChild(std::make_unique<juce::AudioParameterFloat>("_Vibrato_Depth4", "DEPTH", 0.0f, 1.0f, 0.5f));
+    vibratoGroup->addChild(std::make_unique<juce::AudioParameterInt>("Vibrato_WF5", "WAVEFORM",  1, 4, 1));
+    vibratoGroup->addChild(std::make_unique<juce::AudioParameterFloat>("Vibrato_Rate6", "RATE", 0.1f, 10.0f, 5.0f));
+    vibratoGroup->addChild(std::make_unique<juce::AudioParameterInt>("Vibrato_Complexity7", "COMPLEXITY", 1, 4, 1));
 	return vibratoGroup;
+}
+
+void Vibrato::prepareToPlay(double sampleRate, int samplesPerBlock)
+{
+    const double smoothTime = 1e-3;
+
+/*
+
+    paramWidth.reset(sampleRate, smoothTime);
+    paramFrequency.reset(sampleRate, smoothTime);
+    paramWaveform.reset(sampleRate, smoothTime);
+    paramInterpolation.reset(sampleRate, smoothTime);
+
+
+    float maxDelayTime = paramWidth.maxValue;
+    delayBufferSamples = (int)(maxDelayTime * (float)sampleRate) + 1;
+    if (delayBufferSamples < 1)
+        delayBufferSamples = 1;
+
+    delayBufferChannels = getTotalNumInputChannels();
+    delayBuffer.setSize(delayBufferChannels, delayBufferSamples);
+    delayBuffer.clear();
+
+    delayWritePosition = 0;
+    lfoPhase = 0.0f;
+    inverseSampleRate = 1.0f / (float)sampleRate;*/
 }
--- Source/vibrato.h
+++ Source/vibrato.h
@@ -11,6 +11,8 @@
 #pragma once
 #include <JuceHeader.h>	
 
+//using twoPi = juce::MathConstants<double>::twoPi;
+
 
 class Vibrato
 {
@@ -22,11 +24,65 @@ public:
 
 private:
 	juce::AudioBuffer<float> delayBuffer;
+    int delayBufferSamples;
+    int delayBufferChannels;
+    int delayWritePosition;
+
+    float lfoPhase;
+    float inverseSampleRate;
+    float twoPi;
+    
     struct lfo
     {
-		float operator()(float phase, int waveform)
+        enum class waveform
+        {
+            Sine,
+            Triangle,
+            Sawtooth,
+            InverseSawtooth,
+            Square
+        };
+        
+        
+        float operator()(float phase, float depth, waveform wf)
 		{
+            float out = 0.0f;
 
+            switch (wf) 
+            {
+                case waveform::Sine : 
+                {
+                    out = 0.5f + 0.5f * sinf(juce::MathConstants<double>::twoPi * phase);
+                    break;
+                }
+                case waveform::Triangle: 
+                {
+                    if (phase < 0.25f)
+                        out = 0.5f + 2.0f * phase;
+                    else if (phase < 0.75f)
+                        out = 1.0f - 2.0f * (phase - 0.25f);
+                    else
+                        out = 2.0f * (phase - 0.75f);
+                    break;
+                }
+                case  waveform::Sawtooth:
+                {
+                    if (phase < 0.5f)
+                        out = 0.5f + phase;
+                    else
+                        out = phase - 0.5f;
+                    break;
+                }
+                case waveform::InverseSawtooth:   
+                {
+                    if (phase < 0.5f)
+                        out = 0.5f - phase;
+                    else
+                         out = 1.5f - phase;
+                    break;
+                }
+            }
+            return out;
 		}
     };
 };
